9438db5df182cf8a2b668fa144a3d2fe8ee94cae
mubashir12321 <mubashir.ahmad71@yahoo.com>
2018-10-18 13:15:14 +0100

    Button css changed

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.css';
-import Person from'./Person/Person';
+import Person from '../components/Persons/Person/Person';
+
 
 
 class App extends Component {
@@ -47,12 +48,13 @@ class App extends Component {
       persons = (
           <div>
               { this.state.persons.map((person,index) => {
-                return <Person
+                return<Person
                     click={()=> this.deletePersonHandler(index)}
                     name = {person.name}
                     age = {person.age}
                     key={person.id}
                     changed={(event)=>this.nameChangedHandler(event,person.id)}/>
+
               })}
 
           </div>
